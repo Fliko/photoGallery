99dba342d1152e33b1e44ec5a70ce76648086fae
Fliko <baxter.flw@gmail.com>
2017-06-22 01:30:24 -0500

    Make basic outdoor gallery

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 var express = require('express');
 var path = require('path');
+
 var app = express();
+
+app.use(express.static(__dirname));
 
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname + '/index.html'));
