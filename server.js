be99844dbd4e8425d9040811c66cf6cd422bca49
Fliko <baxter.flw@gmail.com>
2017-06-25 15:53:29 -0500

    Put together full mvp

--- server.js
+++ server.js
@@ -13,12 +13,12 @@ app.get('/gallery', (req, res) => {
   res.sendFile(path.join(__dirname + '/featured.html'));
 });
 
-// app.get('/about', (req, res) => {
-//   res.sendFile(path.join(__dirname + '/about.html'));
-// });
-//
-// app.get('/all', (req, res) => {
-//   res.sendFile(path.join(__dirname + '/all.html'));
-// });
+app.get('/about', (req, res) => {
+  res.sendFile(path.join(__dirname + '/about.html'));
+});
+
+app.get('/all', (req, res) => {
+  res.sendFile(path.join(__dirname + '/all.html'));
+});
 
 app.listen(3034, () => console.log('listening on 8080'));
