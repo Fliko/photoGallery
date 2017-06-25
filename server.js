d2aed848f15d1035fa672b91209fbbc6504c8547
Fliko <baxter.flw@gmail.com>
2017-06-25 15:46:09 -0500

    Start about and full gallery pages

--- server.js
+++ server.js
@@ -10,7 +10,15 @@ app.get('/', (req, res) => {
 });
 
 app.get('/gallery', (req, res) => {
-  res.sendFile(path.join(__dirname + '/index.html'));
+  res.sendFile(path.join(__dirname + '/featured.html'));
 });
 
-app.listen(8080, () => console.log('listening on 8080'));
+// app.get('/about', (req, res) => {
+//   res.sendFile(path.join(__dirname + '/about.html'));
+// });
+//
+// app.get('/all', (req, res) => {
+//   res.sendFile(path.join(__dirname + '/all.html'));
+// });
+
+app.listen(3034, () => console.log('listening on 8080'));
